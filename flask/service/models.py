a6230a30a9e62924bfee16767a4ece8a2cb4c864
ohahohah <ohahohah.dev@gmail.com>
2019-11-04 19:29:59 +0900

    Add ch03

--- flask/service/models.py
+++ flask/service/models.py
@@ -2,6 +2,8 @@ from marshmallow import Schema, fields, pre_load
 from marshmallow import validate
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from passlib.apps import custom_app_context as password_context
+import re
 
 orm = SQLAlchemy()
 ma = Marshmallow()
@@ -20,6 +22,36 @@ class ResourceAddUpdateDelete():
         return orm.session.commit()
 
 
+class User(orm.Model, ResourceAddUpdateDelete):
+    id = orm.Column(orm.Integer, primary_key=True)
+    name = orm.Column(orm.String(50), unique=True, nullable=False)
+    # I save the hash for the password (I don't persist the actual password)
+    password_hash = orm.Column(orm.String(120), nullable=False)
+    creation_date = orm.Column(orm.TIMESTAMP, server_default=orm.func.current_timestamp(), nullable=False)
+
+    def verify_password(self, password):
+        return password_context.verify(password, self.password_hash)
+
+    def check_password_strength_and_hash_if_ok(self, password):
+        if len(password) < 8:
+            return 'The password is too short. Please, specify a password with at least 8 characters.', False
+        if len(password) > 32:
+            return 'The password is too long. Please, specify a password with no more than 32 characters.', False
+        if re.search(r'[A-Z]', password) is None:
+            return 'The password must include at least one uppercase letter.', False
+        if re.search(r'[a-z]', password) is None:
+            return 'The password must include at least one lowercase letter.', False
+        if re.search(r'\d', password) is None:
+            return 'The password must include at least one number.', False
+        if re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', password) is None:
+            return 'The password must include at least one symbol.', False
+        self.password_hash = password_context.hash(password)
+        return '', True
+
+    def __init__(self, name):
+        self.name = name
+
+
 class Notification(orm.Model, ResourceAddUpdateDelete):
     id = orm.Column(orm.Integer, primary_key=True)
     message = orm.Column(orm.String(250), unique=True, nullable=False)
@@ -33,6 +65,17 @@ class Notification(orm.Model, ResourceAddUpdateDelete):
     displayed_times = orm.Column(orm.Integer, nullable=False, server_default='0')
     displayed_once = orm.Column(orm.Boolean, nullable=False, server_default='false')
 
+    @classmethod
+    def is_message_unique(cls, id, message):
+        existing_notification = cls.query.filter_by(message=message).first()
+        if existing_notification is None:
+            return True
+        else:
+            if existing_notification.id == id:
+                return True
+            else:
+                return False
+
     def __init__(self, message, ttl, notification_category):
         self.message = message
         self.ttl = ttl
@@ -42,6 +85,17 @@ class Notification(orm.Model, ResourceAddUpdateDelete):
 class NotificationCategory(orm.Model, ResourceAddUpdateDelete):
     id = orm.Column(orm.Integer, primary_key=True)
     name = orm.Column(orm.String(150), unique=True, nullable=False)
+
+    @classmethod
+    def is_name_unique(cls, id, name):
+        existing_notification_category = cls.query.filter_by(name=name).first()
+        if existing_notification_category is None:
+            return True
+        else:
+            if existing_notification_category.id == id:
+                return True
+            else:
+                return False
 
     def __init__(self, name):
         self.name = name
@@ -89,3 +143,12 @@ class NotificationSchema(ma.Schema):
             notification_category_dict = {}
         data['notification_category'] = notification_category_dict
         return data
+
+
+class UserSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True,
+                         validate=validate.Length(3))
+    url = ma.URLFor('service.userresource',
+                    id='<id>',
+                    _external=True)
--- flask/service/views.py
+++ flask/service/views.py
@@ -3,14 +3,84 @@ from flask_restful import Api, Resource
 from http_status import HttpStatus
 from models import orm, NotificationCategory, NotificationCategorySchema, Notification, NotificationSchema
 from sqlalchemy.exc import SQLAlchemyError
+from helpers import PaginationHelper
+from flask_httpauth import HTTPBasicAuth
+from flask import g
+from models import User, UserSchema
 
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_user_password(name, password):
+    user = User.query.filter_by(name=name).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+
+class AuthenticationRequiredResource(Resource):
+    method_decorators = [auth.login_required]
+
+
+user_schema = UserSchema()
 service_blueprint = Blueprint('service', __name__)
 notification_category_schema = NotificationCategorySchema()
 notification_schema = NotificationSchema()
 service = Api(service_blueprint)
 
 
-class NotificationResource(Resource):
+class UserResource(AuthenticationRequiredResource):
+    def get(self, id):
+        user = User.query.get_or_404(id)
+        result = user_schema.dump(user).data
+        return result
+
+
+class UserListResource(Resource):
+    @auth.login_required
+    def get(self):
+        pagination_helper = PaginationHelper(
+            request,
+            query=User.query,
+            resource_for_url='service.userlistresource',
+            key_name='results',
+            schema=user_schema)
+        result = pagination_helper.paginate_query()
+        return result
+
+    def post(self):
+        user_dict = request.get_json()
+        if not user_dict:
+            response = {'user': 'No input data provided'}
+            return response, HttpStatus.bad_request_400.value
+        errors = user_schema.validate(user_dict)
+        if errors:
+            return errors, HttpStatus.bad_request_400.value
+        user_name = user_dict['name']
+        existing_user = User.query.filter_by(name=user_name).first()
+        if existing_user is not None:
+            response = {'user': 'An user with the name {} already exists'.format(user_name)}
+            return response, HttpStatus.bad_request_400.value
+        try:
+            user = User(name=user_name)
+            error_message, password_ok = \
+                user.check_password_strength_and_hash_if_ok(user_dict['password'])
+            if password_ok:
+                user.add(user)
+                query = User.query.get(user.id)
+                dump_result = user_schema.dump(query).data
+                return dump_result, HttpStatus.created_201.value
+            else:
+                return {"error": error_message}, HttpStatus.bad_request_400.value
+        except SQLAlchemyError as e:
+            orm.session.rollback()
+            response = {"error": str(e)}
+            return response, HttpStatus.bad_request_400.value
+
+
+class NotificationResource(AuthenticationRequiredResource):
     def get(self, id):
         notification = Notification.query.get_or_404(id)
         dumped_notification = notification_schema.dump(notification).data
@@ -19,15 +89,20 @@ class NotificationResource(Resource):
     def patch(self, id):
         notification = Notification.query.get_or_404(id)
         notification_dict = request.get_json(force=True)
+        print(notification_dict)
         if 'message' in notification_dict and notification_dict['message'] is not None:
-            notification.message = notification_dict['message']
+            notification_message = notification_dict['message']
+            if not Notification.is_message_unique(id=0, message=notification_message):
+                response = {'error': 'A notification with the message {} already exists'.format(notification_message)}
+                return response, HttpStatus.bad_request_400.value
+            notification.message = notification_message
         if 'ttl' in notification_dict and notification_dict['ttl'] is not None:
             notification.duration = notification_dict['duration']
         if 'displayed_times' in notification_dict and notification_dict['displayed_times'] is not None:
             notification.displayed_times = notification_dict['displayed_times']
         if 'displayed_once' in notification_dict and notification_dict['displayed_once'] is not None:
-            notification.displayed_once = notification_dict['displayed_once']
-        dumped_notification, dump_errors = notification_schema.dump(message)
+            notification.displayed_once = notification_dict['displayed_once'] == 'true'
+        dumped_notification, dump_errors = notification_schema.dump(notification)
         if dump_errors:
             return dump_errors, HttpStatus.bad_request_400.value
         validate_errors = notification_schema.validate(dumped_notification)
@@ -53,11 +128,16 @@ class NotificationResource(Resource):
             return response, HttpStatus.unauthorized_401.value
 
 
-class NotificationListResource(Resource):
+class NotificationListResource(AuthenticationRequiredResource):
     def get(self):
-        notifications = Notification.query.all()
-        dump_result = notification_schema.dump(notifications, many=True).data
-        return dump_result
+        pagination_helper = PaginationHelper(
+            request,
+            query=Notification.query,
+            resource_for_url='service.notificationlistresource',
+            key_name='results',
+            schema=notification_schema)
+        pagination_result = pagination_helper.paginate_query()
+        return pagination_result
 
     def post(self):
         notification_category_dict = request.get_json()
@@ -67,6 +147,10 @@ class NotificationListResource(Resource):
         errors = notification_schema.validate(notification_category_dict)
         if errors:
             return errors, HttpStatus.bad_request_400.value
+        notification_message = notification_category_dict['message']
+        if not Notification.is_message_unique(id=0, message=notification_message):
+            response = {'error': 'A notification with the message {} already exists'.format(notification_message)}
+            return response, HttpStatus.bad_request_400.value
         try:
             notification_category_name = notification_category_dict['notification_category']['name']
             notification_category = NotificationCategory.query.filter_by(name=notification_category_name).first()
@@ -77,7 +161,7 @@ class NotificationListResource(Resource):
             # Now that we are sure we have a notification category,
             # we can create a new Notification
             notification = Notification(
-                message=notification_category_dict['message'],
+                message=notification_message,
                 ttl=notification_category_dict['ttl'],
                 notification_category=notification_category)
             notification.add(notification)
@@ -90,7 +174,7 @@ class NotificationListResource(Resource):
             return response, HttpStatus.bad_request_400.value
 
 
-class NotificationCategoryResource(Resource):
+class NotificationCategoryResource(AuthenticationRequiredResource):
     def get(self, id):
         notification_category = NotificationCategory.query.get_or_404(id)
         dump_result = notification_category_schema.dump(notification_category).data
@@ -107,7 +191,13 @@ class NotificationCategoryResource(Resource):
             return errors, HttpStatus.bad_request_400.value
         try:
             if 'name' in notification_category_dict and notification_category_dic['name'] is not None:
-                notification_category.name = notification_category_dict['name']
+                notification_category_name = notification_category_dict['name']
+                if NotificationCategory.is_name_unique(id=id, name=notification_category_name):
+                    notification_category.name = notification_category_name
+                else:
+                    response = {
+                        'error': 'A category with the name {} already exists'.format(notification_category_name)}
+                    return response, HttpStatus.bad_request_400.value
             notification_category.update()
             return self.get(id)
         except SQLAlchemyError as e:
@@ -127,7 +217,7 @@ class NotificationCategoryResource(Resource):
             return response, HttpStatus.unauthorized_401.value
 
 
-class NotificationCategoryListResource(Resource):
+class NotificationCategoryListResource(AuthenticationRequiredResource):
     def get(self):
         notification_categories = NotificationCategory.query.all()
         dump_results = notification_category_schema.dump(notification_categories, many=True).data
@@ -142,8 +232,13 @@ class NotificationCategoryListResource(Resource):
         errors = notification_category_schema.validate(notification_category_dict)
         if errors:
             return errors, HttpStatus.bad_request_400.value
+        notification_category_name = notification_category_dict['name']
+        if not NotificationCategory.is_name_unique(id=0, name=notification_category_name):
+            response = {
+                'error': 'A notification category with the name {} already exists'.format(notification_category_name)}
+            return response, HttpStatus.bad_request_400.value
         try:
-            notification_category = NotificationCategory(notification_category_dict['name'])
+            notification_category = NotificationCategory(notification_category_name)
             notification_category.add(notification_category)
             query = NotificationCategory.query.get(notification_category.id)
             dump_result = notification_category_schema.dump(query).data
@@ -164,3 +259,7 @@ service.add_resource(NotificationListResource,
                      '/notifications/')
 service.add_resource(NotificationResource,
                      '/notifications/<int:id>')
+service.add_resource(UserListResource,
+                     '/users/')
+service.add_resource(UserResource,
+                     '/users/<int:id>')
